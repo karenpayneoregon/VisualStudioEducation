0d0f93bf0a7a42e17ec53e0a6b7e254ba4e7ee35
Karen Payne <paynekaren@comcast.net>
2019-06-05 14:39:30 -0700

    Customer partial class

---
+++ NorthWindEntityLibrary/PartialClasses/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedLibrary.Interfaces;
+
+namespace NorthWindEntityLibrary
+{
+    public partial class Customer : IBaseEntity
+    {
+        public int Id => CustomerIdentifier;
+        public int ModifiedByUserId => 0;
+
+        // ReSharper disable once PossibleInvalidOperationException
+        DateTime IBaseEntity.ModifiedDate => ModifiedDate.Value;
+    }
+}
